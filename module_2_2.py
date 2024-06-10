ec2a511f6158bcbd45991fe2a1730183c1c52b27
Olga5515 <torfenita@mail.ru>
2024-06-11 08:38:48 +1000

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+first = 158976
+second = 158976
+third = 158976
+if first == second == third:
+    print(3)
+elif first == second:
+    print(2)
+elif first == third:
+    print(2)
+elif second == third:
+    print(2)
+else:
+    print(0)
+
+# Условная конструкция. Операторы if, elif, else. Д/з сдано 2024/06/11 08:40
